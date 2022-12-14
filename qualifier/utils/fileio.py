6c59f684606badc738fbf7581c704bd263669a73
Vincent Sgarzi <vincentsgarzi@gmail.com>
2022-12-14 18:04:40 -0500

    implemented the save_csm method

--- qualifier/utils/fileio.py
+++ qualifier/utils/fileio.py
@@ -5,7 +5,7 @@ This contains a helper function for loading and saving CSV files.
 
 """
 import csv
-
+from pathlib import Path
 
 def load_csv(csvpath):
     """Reads the CSV file from path provided.
@@ -28,3 +28,27 @@ def load_csv(csvpath):
         for row in csvreader:
             data.append(row)
     return data
+
+
+def save_csv(data, outputPath):
+    """save_csv saves data to a csv file
+    Args:
+        data (list of lists): list of loans
+
+    Returns:
+        csvPath (string): path to CSV file
+    """
+    #headers for the csv file
+    headers = ["Lender", "Max Loan Amount", "Max LTV", "Max DTI", "Max Credit Score", "Interest Rate"]
+
+
+
+    #open the csv gathered from prompt, write in the headers and data
+    with open(outputPath, "w", newline='') as csvFile:
+
+        csvWriter = csv.writer(csvFile, delimiter = ",")
+        csvWriter.writerow(headers)
+        for row in data:
+            csvWriter.writerow(row)
+
+    return outputPath
